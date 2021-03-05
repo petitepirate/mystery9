8318fdb91ac02c6e9506fc06ef131df22f55e2c1
megan <msmeganmcmanus@gmail.com>
2021-03-04 19:26:36 -0800

    complete unroll

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,15 @@
 function unroll(squareArray) {
+const newArr = [];
 
+while (squareArray.length) {
+    newArr.push(
+        ...squareArray.shift(),
+        ...squareArray.map(a => a.pop()),
+        ...(squareArray.pop() || []).reverse(),
+        ...squareArray.map(a => a.shift()).reverse()
+    );
+}
+return newArr;
 }
 
 module.exports = unroll;
