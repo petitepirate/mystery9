3fa14bac3c942917db0c3152657ae8390ae11901
megan <msmeganmcmanus@gmail.com>
2021-03-05 16:31:32 -0800

    add documentation

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,12 +1,16 @@
 function unroll(squareArray) {
 const newArr = [];
 
-while (squareArray.length) {
+while (squareArray.length) { //prevent infinite loop
     newArr.push(
+        // step 1: return the first array within the matrix [1,2,3,4] w/ spread operator allows us to recieve it like: 1 2 3 4
         ...squareArray.shift(),
-        ...squareArray.map(a => a.pop()),
+        // step 2: map over each array using the pop method to recieve the very last number in each array
+        ...squareArray.map(arr => arr.pop()),
+        // step 3: similar to step 1, but also adding reverse 
         ...(squareArray.pop() || []).reverse(),
-        ...squareArray.map(a => a.shift()).reverse()
+        // step 4: similar to step 2, map over remaining arrays in reverse, pulling off the first number and retruning them.
+        ...squareArray.map(arr => arr.shift()).reverse()
     );
 }
 return newArr;
